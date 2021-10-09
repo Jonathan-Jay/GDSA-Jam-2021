104886b34293a2b8b5902abca02a6ef42fc58420
Michael Wilson <ebonyprince2014@gmail.com>
2021-10-09 02:24:05 -0400

    Update CarManager.cs

--- Pizza Parker/Assets/Scripts/CarManager.cs
+++ Pizza Parker/Assets/Scripts/CarManager.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public class CarManager : MonoBehaviour
 {
-    public struct SpawnData
-    {
-        Transform spawnPoint;
-        Vector2 direction;
-    }
 
-    public List<SpawnData> spawnPoints;
     // Start is called before the first frame update
     void Start()
     {
