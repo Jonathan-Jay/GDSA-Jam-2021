205d262d642873478ee9c9a71cb8b0ceb206b330
Michael Wilson <ebonyprince2014@gmail.com>
2021-10-09 15:13:47 -0400

    Added Spawn Script

--- Pizza Parker/Assets/Scripts/CarManager.cs
+++ Pizza Parker/Assets/Scripts/CarManager.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class CarManager : MonoBehaviour
 {
+    public float minCountdown = 5;
+    public float maxCountdown = 10;
+    private float countdown;
+    public GameObject carPrefab;
 
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public struct SpawnPoint
     {
-        
+        public Transform position;
+        public Vector2 direction;
     }
 
-    // Update is called once per frame
+    public List<SpawnPoint> spawnPoints;
+
+    void Start()
+    {
+        countdown = Random.Range(minCountdown, maxCountdown);
+    }
+
     void Update()
     {
-        
+        //randomized counter system
+        countdown -= Time.deltaTime;
+        if (countdown < 0)
+        {
+            countdown = Random.Range(minCountdown, maxCountdown);
+            //spawn system
+            int index = Random.Range(0, spawnPoints.Count);
+            Instantiate(carPrefab, spawnPoints[index].position.position, Quaternion.identity)
+                .GetComponent<Cars>().dir = spawnPoints[index].direction;
+        }
     }
 }
