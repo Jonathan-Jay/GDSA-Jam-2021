3c707b6971cc5f6b10932e69803ea9827a2d8d10
Jonathan Jay <jonathan.jay@ontariotech.net>
2021-10-10 03:56:44 -0400

    mvoed some files and switched to 3d physics
    also player works now

--- Pizza Parker/Assets/Scripts/Cars.cs
+++ Pizza Parker/Assets/Scripts/Cars.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Forces game object to have rigidbody2D component
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody))]
 public class Cars : MonoBehaviour
 {
-    Rigidbody2D rb;
+    Rigidbody rb;
 
-    public Vector2 dir = Vector2.up;
+    public Vector3 dir = Vector3.forward;
     public float speed = 5;
     public float knockback = 10;
+	private float stunTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody>();
+
+		stunTime = 1f / speed;
     }
 
     // Update is called once per frame
@@ -28,15 +32,15 @@ public class Cars : MonoBehaviour
         //If dir or speed = 0, car will stop moving
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter(Collision collision)
     {
         //Layer 6 is player layer
         if (collision.gameObject.layer == 6)
         {
             //variable for knockback direction based on collision point
-            Vector2 knockbackDir = (collision.GetContact(0).point - (Vector2)transform.position).normalized;
+            Vector3 knockbackDir = (collision.GetContact(0).point - transform.position).normalized;
             collision.rigidbody.velocity = knockbackDir * knockback;
-
+			collision.gameObject.GetComponent<Player>().stunned = stunTime;
         }
     }
 }
